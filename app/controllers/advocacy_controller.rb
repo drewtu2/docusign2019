a2845e9e90d1acd8b3d885a6f2edefe4f7ed3a5b
Ashwin Subramanian <Ashwin.Subramanian@ooma.com>
2019-06-09 14:35:21 -0700

    last again

--- app/controllers/advocacy_controller.rb
+++ app/controllers/advocacy_controller.rb
@@ -1,18 +1,19 @@
 class AdvocacyController < ApplicationController
   def index
+
+    @thank = "false"
     if session[:supporting_issue].present?
       @thank = "true"
       @supporting_issue = session.delete(:supporting_issue)
+
+
       Advocacy.create(user_id: session[:user_id], issue_id: session[:issue_id])
       result = `python3 ./lib/scripts/get_reps.py --zip "50014"`
       # Get senator details
       @sent_to = "Senator #{result.split("\n").first.split(",")[-3].split("'").last} at #{result.split("\n").first.split(",").last.split("'")[1]}"
     end
+   puts "#{@thank}"
 
-    if params[:donation].present?
-      @thank = "true"
-      @donation = "true"
-    end
     @issues = Issue.all
     @supported = User.find(session[:user_id]).advocacies.map(&:issue_id)
   end
