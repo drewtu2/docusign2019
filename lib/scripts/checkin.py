873489abbf5d192c2f583ed624f3ba2abaa60816
Andrew Tu <drewtu2@yahoo.com>
2019-06-09 11:07:03 -0700

    fixed checkin

--- lib/scripts/checkin.py
+++ lib/scripts/checkin.py
@@ -2,6 +2,7 @@
 from DatabaseGateway import DatabaseGateway
 from location import get_nearest_park
 from datetime import datetime
+import argparse
 
 #For when the front-end clicks the check-in button
 def save_checkin(latitude, longitude):
@@ -10,3 +11,12 @@ def save_checkin(latitude, longitude):
 	output = get_nearest_park(latitude,longitude)
 	print(output)
 	db.check_in_user('000', output, time)
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument('--lat', type=str, help='Lat')
+    parser.add_argument('--long', type=str, help='Long')
+    args=parser.parse_args()
+    
+    print(save_checkin(args.lat, args.long))
